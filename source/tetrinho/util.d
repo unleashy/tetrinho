1a0309d29ca490fb97dd7e4e23319d249d7ce090
unleashy <lakeavenger@gmail.com>
2018-02-09 19:43:58 -0200

    Add Color tuple and Colors struct

--- source/tetrinho/util.d
+++ source/tetrinho/util.d
@@ -5,6 +5,18 @@ import derelict.sdl2.sdl;
 import std.typecons;
 
 alias Coord = Tuple!(int, "x", int, "y");
+alias Color = Tuple!(ubyte, "r", ubyte, "b", ubyte, "g");
+
+enum Colors : Color
+{
+    CYAN   = Color(  0, 255, 255),
+    BLUE   = Color(  0,   0, 255),
+    ORANGE = Color(255, 165,   0),
+    YELLOW = Color(255, 255,   0),
+    GREEN  = Color(  0, 255,   0),
+    PURPLE = Color(128,   0, 128),
+    RED    = Color(255,   0,   0)
+}
 
 T enforceSDL(alias cmp = "a == 0", T)(T a, lazy string message = "SDL error: %s") @trusted
 {
