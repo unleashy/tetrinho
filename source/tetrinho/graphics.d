519025e23e31545910415d69d3e953a19815f304
unleashy <lakeavenger@gmail.com>
2018-02-14 14:39:15 -0200

    Change back to blended ttf rendering lol

--- source/tetrinho/graphics.d
+++ source/tetrinho/graphics.d
@@ -134,7 +134,7 @@ struct Graphics
     private SDL_Texture* renderTextUncached(in string t)
     {
         auto sfc = enforceSDL!"a !is null"(
-            TTF_RenderText_Shaded(mainFont_, t.toStringz, Colors.WHITE, Colors.BLACK)
+            TTF_RenderText_Blended(mainFont_, t.toStringz, Colors.WHITE)
         );
         scope(exit) SDL_FreeSurface(sfc);
 
